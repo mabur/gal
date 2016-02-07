1c975b75da3441c18fe12869464b12eb0592d958
mabur <magnus.burenius@gmail.com>
2016-02-07 22:58:35 +0100

    Forward declare data and extents functions

--- include/non_member_functions.hpp
+++ include/non_member_functions.hpp
@@ -19,3 +19,25 @@ template<typename T, size_t N> const T* data(const std::array<T, N>& x) { return
 
 template<typename Array>
 std::array<size_t, 1> extents(const Array& a) { return std::array<size_t, 1>{a.size()}; }
+
+// Forward declare data and extents functions for the arrays:
+
+template<typename T, size_t RANK> class darray;
+template<typename T, size_t D> T* data(darray<T, D>& a);
+template<typename T, size_t D> const T* data(const darray<T, D>& a);
+template<typename T, size_t D> std::array<size_t, D> extents(const darray<T, D>& a);
+
+template<typename T, size_t RANK> class pdarray;
+template<typename T, size_t D> T* data(pdarray<T, D>& a);
+template<typename T, size_t D> const T* data(const pdarray<T, D>& a);
+template<typename T, size_t D> std::array<size_t, D> extents(const pdarray<T, D>& a);
+
+template<typename T, size_t... EXTENTS> class sarray;
+template<typename T, size_t... EXTENTS> T* data(sarray<T, EXTENTS...>& a);
+template<typename T, size_t... EXTENTS> const T* data(const sarray<T, EXTENTS...>& a);
+template<typename T, size_t... EXTENTS> std::array<size_t, sizeof...(EXTENTS)> extents(const sarray<T, EXTENTS...>& a);
+
+template<typename T, size_t... EXTENTS> class psarray;
+template<typename T, size_t... EXTENTS> T* data(psarray<T, EXTENTS...>& a);
+template<typename T, size_t... EXTENTS> const T* data(const psarray<T, EXTENTS...>& a);
+template<typename T, size_t... EXTENTS> std::array<size_t, sizeof...(EXTENTS)> extents(const psarray<T, EXTENTS...>& a);
